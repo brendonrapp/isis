7723531773914d1c7f1c729eef3db999b149ee4a
Jeremy Ebler <jebler@gmail.com>
2012-02-16 21:41:22 -0800

    enhance !xkcd commands
    !xkcd
    	new, last: current comic
    	random: random comic
    	(number-of-comic), 1, 314: display comic by number
    	first: comic #1
    	next, prev: browse through comics
    	date: get date of last comic
    	number: get number of last comic

--- lib/isis/plugins/xkcd.rb
+++ lib/isis/plugins/xkcd.rb
@@ -20,25 +20,71 @@ class Isis::Plugin::XKCD < Isis::Plugin::Base
     verb = @commands[1]
 
     return case
-    when verb.nil?
-      new_comic
-    when verb.to_i.to_s == verb
-      selected_comic(verb)
-    when verb == "random"
-      random_comic
-    when verb == "new"
-      new_comic
-    when (verb == "commands" or verb == "help")
-      "Understood command arguments for !xkcd: new, random, (number-of-comic)"
+    when !@dbready
+      ["Hold up, I'm not ready yet."]
+
+    when (!defined?(verb) or verb.blank? or verb == "random")
+      load_comic 'http://dynamic.xkcd.com/random/comic/'
+
+    when (verb == "new" or verb == "last")
+      load_comic 'http://xkcd.com/'
+
+    when verb.to_i.to_s == verb # When integer
+      selected_comic(verb.to_i)
+
+    when verb == "first"
+      selected_comic 1
+
+    when verb == "next"
+      @last_comic += 1
+      selected_comic(@last_comic)
+
+    when verb == "prev"
+      @last_comic -= 1
+      selected_comic(@last_comic)
+
+    when verb == "date"
+      @db[:comics].where(:id => @last_comic).select(:date).first[:date]
+
+    when verb == "number"
+      @last_comic.to_s
+
+    when verb == "d"
+      "COMIC index: #{@last_comic}, count: #{@db[:comics].count}, max: #{@db[:comics].max(:id)}, min: #{@db[:comics].min(:id)}"
+
+    when (verb == "commands" or verb == "help" or verb == "--help" or verb == "-h")
+      "Understood command arguments for !xkcd:\n"+
+      "\tnew, last: current comic\n"+
+      "\trandom: ??\n"+
+      "\t(number-of-comic), 1, 314: display comic by number\n"+
+      "\tfirst: comic #1\n"+
+      "\tnext, prev: browse through comics\n"+
+      "\tdate: get date of last comic\n"+
+      "\tnumber: get number of last comic"
+
     else
-      "I have no idea what #{verb} means. No comic for you."
+      "I have no idea what #{verb} means. Please come again."
+
     end
   end
 
-  def new_comic
-    page = Nokogiri::HTML(open('http://xkcd.com/'))
-    image = page.css('.s > img').first
-    [image['src'], image['title']]
+
+  def selected_comic(number)
+    case
+    when(number <= @db[:comics].max(:id) and number >= @db[:comics].min(:id))
+      # @last_comic = number
+      load_comic "http://xkcd.com/#{number}/"
+
+    when number < 0
+      ["negative comic number? really?"]
+
+    when number == 0
+      ["THERE IS NO COMIC ZERO"]
+
+    else # number > comic.max
+      ["xkcd has produced #{@db[:comics].max(:id)} comics. try one of those."]
+
+    end
   end
 
 private
@@ -48,7 +94,7 @@ private
         @last_comic = match.captures.first.to_i
 
         image = page.at('#middleContent .s img')
-        ["broken"+image['src'], image['title']]
+        [image['src'], image['title']]
       else
         "404??"
     end
